e529b9f9e07bf6dda9e70ffc76551e78a473def7
pctronique <pctronique@free.fr>
2023-09-20 21:15:51 +0200

    démarrage automatique du server

--- .docker/symfony/updateProject.sh
+++ .docker/symfony/updateProject.sh
@@ -3,4 +3,4 @@ do
    export $line
 done < ${0%/*}/.env
 cd /home/project/$FOLDER_PROJECT_SYMFONY
-symfony server:start >> /var/log/symfony/symfony_out.log 2>> /var/log/symfony/symfony_error.log
+symfony server:$1 --no-tls -d >> /var/log/symfony/symfony_out.log 2>> /var/log/symfony/symfony_error.log
