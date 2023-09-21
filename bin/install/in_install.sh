b09ff77858db6cda69f87ea162db761a45adf30f
pctronique <pctronique@free.fr>
2023-09-21 22:50:18 +0200

    modif in_install

--- bin/install/in_install.sh
+++ bin/install/in_install.sh
@@ -5,8 +5,8 @@ do
    export $line
 done < ${0%/*}/../../.env
 
-LINE1="- .\/.docker\/config\/.env.local:\/home\/project\/www\/.env.local:ro"
-LINE1_REPLACE="- .\/.docker\/config\/.env.local.example:\/home\/project\/$FOLDER_PROJECT_SYMFONY\/.env.local.example:ro"
+LINE1="- .\/.docker\/config\/.env.local.example:\/home\/project\/www\/.env.local:ro"
+LINE1_REPLACE="- .\/.docker\/config\/.env.local.example:\/home\/project\/$FOLDER_PROJECT_SYMFONY\/.env.local:ro"
 DESACTIVE="#"
 FILE="${0%/*}/../../docker-compose.yml"
 
