6037ed166c6aded110a4a95a89491ef510317b38
pctronique <pctronique@free.fr>
2024-05-14 16:04:09 +0200

    modif cron

--- .docker/images/php/docker-create-symfony-entrypoint.sh
+++ .docker/images/php/docker-create-symfony-entrypoint.sh
@@ -5,6 +5,11 @@ then
     PHP_FOLDER_LOG="/var/log/docker/php/"
 fi
 
+if [ -z ${CRON_FOLDER_INIT} ]
+then
+    CRON_FOLDER_INIT="/var/docker/cron/"
+fi
+
 if [ -z ${PHP_FOLDER_INIT} ]
 then
     PHP_FOLDER_INIT="/var/docker/php/"
@@ -21,8 +26,15 @@ ${PHP_FOLDER_INIT}/createconnsgbd.sh 2>> ${PHP_FOLDER_LOG}/createconnsgbd.log
 
 ${PHP_FOLDER_INIT}/importdata.sh 2>> ${PHP_FOLDER_LOG}/installdata.log
 
+cp ${CRON_FOLDER_INIT}/dockercron /etc/cron.d/dockercron
+
 crontab /etc/cron.d/dockercron
-while inotifywait -e close_write /etc/cron.d/dockercron; do crontab /etc/cron.d/dockercron; done &
+
+#while inotifywait -e close_write /etc/cron.d/dockercron; do crontab /etc/cron.d/dockercron; done &
+
+crontab /etc/cron.d/dockercron
+
+${CRON_FOLDER_INIT}/cronauto.sh 2>> ${PHP_FOLDER_LOG}/initnodejs.log &
 
 touch ${PHP_FOLDER_LOG}/cron.log
 cron && tail -f ${PHP_FOLDER_LOG}/cron.log &
